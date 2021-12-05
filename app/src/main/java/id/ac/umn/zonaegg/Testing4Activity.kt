1af8b23ceff5fec76a108744d39a750669ca28a9
nehemiacecio <nehemia.cecio.3@gmail.com>
2021-12-05 23:06:52 +0700

    Firestore Retrieve Data

--- app/src/main/java/id/ac/umn/zonaegg/Testing4Activity.kt
+++ app/src/main/java/id/ac/umn/zonaegg/Testing4Activity.kt
@@ -2,10 +2,40 @@ package id.ac.umn.zonaegg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 
 class Testing4Activity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var userArrayList: RecyclerView
+    private lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_testing4)
+
+    }
+
+    private fun EventChangeListener(){
+
+        db= FirebaseFirestore.getInstance()
+        db.collection("").
+                addSnapshotListener(object : EventListener<QuerySnapshot>{
+                    override fun onEvent(
+                        value: QuerySnapshot?,
+                        error: FirebaseFirestoreException?
+                    ){
+
+
+
+                    }
+                })
     }
 }
